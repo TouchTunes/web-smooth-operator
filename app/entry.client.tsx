d78acb7442e59ce3158ff28531c05b1fbf053706
y-chernenko <c-ychernenko@touchtunes.com>
2025-03-24 17:31:45 +0200

    Set up MUI

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -4,15 +4,57 @@
  * For more information, see https://remix.run/file-conventions/entry.client
  */
 
+import { useMemo, useState } from "react";
+import { CacheProvider } from "@emotion/react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
 import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
-startTransition(() => {
+import theme from "./theme";
+import ClientStyleContext from "./ClientStyleContext";
+import createEmotionCache from "./createEmotionCache";
+
+interface ClientCacheProviderProps {
+  children: React.ReactNode;
+}
+
+function ClientCacheProvider({ children }: ClientCacheProviderProps) {
+  const [cache, setCache] = useState(createEmotionCache());
+
+  const clientStyleContextValue = useMemo(
+    () => ({
+      reset() {
+        setCache(createEmotionCache());
+      },
+    }),
+    []
+  );
+
+  return (
+    <ClientStyleContext.Provider value={clientStyleContextValue}>
+      <CacheProvider value={cache}>{children}</CacheProvider>
+    </ClientStyleContext.Provider>
+  );
+}
+
+const hydrateFn = () => {
   hydrateRoot(
     document,
-    <StrictMode>
-      <RemixBrowser />
-    </StrictMode>
+    <ClientCacheProvider>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <RemixBrowser />
+      </ThemeProvider>
+    </ClientCacheProvider>
   );
-});
+};
+
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(hydrateFn);
+} else {
+  // Safari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  window.setTimeout(hydrateFn, 1);
+}
