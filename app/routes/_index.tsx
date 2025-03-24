243367866e6460884a777eb2a6b6d5ab945535c6
y-chernenko <c-ychernenko@touchtunes.com>
2025-03-24 20:17:29 +0200

    remove redundant meta func

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,4 @@
-import { type MetaFunction } from '@remix-run/node';
 import { redirect } from '@remix-run/react';
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: 'Smooth Operator' },
-    { name: 'description', content: 'Welcome to Remix!' },
-  ];
-};
 
 export function loader() {
   return redirect('/dashboard');
