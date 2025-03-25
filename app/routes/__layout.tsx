d37260960489e0a012c081e1adf55f776a77ab65
y-chernenko <c-ychernenko@touchtunes.com>
2025-03-25 12:49:50 +0200

    Side panel

--- app/routes/__layout.tsx
+++ app/routes/__layout.tsx
@@ -1,32 +1,59 @@
-import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import AssessmentIcon from '@mui/icons-material/Assessment';
+import {
+  Box,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+} from '@mui/material';
 import { Link, Outlet } from '@remix-run/react';
 
 import type { MetaFunction } from '@remix-run/react';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'Smooth Operator' }];
+  return [{ title: 'TouchTunes Smooth Operator' }];
 };
 
 function Layout() {
   return (
     <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography
-              variant="h6"
-              component={Link}
-              to="/dashboard"
-              sx={{ flexGrow: 1, color: 'text.primary' }}
-            >
-              Smooth Operator
-            </Typography>
-            <Button component={Link} to="/operators" color="inherit">
-              Operators
-            </Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
+      <Drawer
+        sx={{
+          width: 250,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: 250,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar>Smooth Operator</Toolbar>
+        <Divider />
+        <List>
+          {['Reports', 'Operators'].map((text, index) => (
+            <ListItem key={index} disablePadding>
+              <ListItemButton
+                component={Link}
+                to={`/${text.toLocaleLowerCase()}`}
+              >
+                <ListItemIcon>
+                  {index % 2 === 0 ? <AssessmentIcon /> : <AccountBoxIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+      </Drawer>
+
       <Box sx={{ mt: 8, p: 2 }}>
         <Outlet />
       </Box>
