cd0050152f1d12c467b407c97964aa73b2c1e092
y-chernenko <c-ychernenko@touchtunes.com>
2025-03-25 12:06:34 +0200

    Operators table

--- app/routes/__layout.operators.tsx
+++ app/routes/__layout.operators.tsx
@@ -1,28 +1,82 @@
-import { Container } from '@mui/material';
-import { json } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { Avatar, Container, Stack, Typography } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import { useFetcher, useLoaderData } from '@remix-run/react';
+import { v4 as uuidv4 } from 'uuid';
 
 import { getOperators } from '~/src/components/services/operators.service';
+
+import type { GridColDef } from '@mui/x-data-grid';
+import type { Operator } from '~/src/components/services/operators.service';
 
 export async function loader() {
   try {
     const operators = await getOperators();
-    return json({
+    return {
       operators,
-    });
+      error: null,
+    };
   } catch (error: unknown) {
-    console.log(error);
+    return {
+      operators: null,
+      error,
+    };
   }
-
-  return json({
-    operators: null,
-  });
 }
 
 export default function Operators() {
   const { operators } = useLoaderData<typeof loader>();
-  console.log(operators);
+  const { state } = useFetcher<typeof loader>();
+  const isLoading = state !== 'idle';
+  const getOperatorName = (operator: Operator) =>
+    `${operator.name.title} ${operator.name.first} ${operator.name.last}`;
+
+  const columns: GridColDef<Operator>[] = [
+    {
+      field: 'fullName',
+      headerName: 'First Name',
+      flex: 1,
+      renderCell: ({ row }) => (
+        <Stack direction={'row'} alignItems="center" sx={{ height: 'inherit' }}>
+          <Avatar
+            alt={getOperatorName(row)}
+            src={row.picture.thumbnail}
+            sx={{ width: 40, height: 40, mr: 2 }}
+          />
+          <Typography>{getOperatorName(row)}</Typography>
+        </Stack>
+      ),
+    },
+    {
+      field: 'cell',
+      headerName: 'Phone',
+      flex: 1,
+    },
+    {
+      field: 'location',
+      headerName: 'Location',
+      flex: 2,
+      valueGetter: (value, row) =>
+        `${row.location.city}, ${row.location.state}, ${row.location.country}`,
+    },
+  ];
+
   return (
-    <Container maxWidth="md" sx={{ textAlign: 'center', mt: 8 }}></Container>
+    <Container maxWidth="md" sx={{ textAlign: 'center', mt: 8 }}>
+      <DataGrid
+        rows={operators || []}
+        columns={columns}
+        loading={isLoading}
+        getRowId={() => uuidv4()}
+        disableRowSelectionOnClick
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+      />
+    </Container>
   );
 }
--- app/src/components/services/operators.service.ts
+++ app/src/components/services/operators.service.ts
@@ -1,7 +1,30 @@
 import axios from 'axios';
 
-const getOperators = async (): Promise<any> => {
-  const response = await axios.get('https://randomuser.me/api/?results=10');
+interface OperatorName {
+  title: string;
+  first: string;
+  last: string;
+}
+
+interface OperatorAvatar {
+  thumbnail: string;
+}
+
+interface OperatorLocation {
+  city: string;
+  state: string;
+  country: string;
+}
+
+export interface Operator {
+  name: OperatorName;
+  cell: string;
+  location: OperatorLocation;
+  picture: OperatorAvatar;
+}
+
+const getOperators = async (): Promise<Operator[]> => {
+  const response = await axios.get('https://randomuser.me/api/?results=100');
   const { data } = response;
   return data.results;
 };
