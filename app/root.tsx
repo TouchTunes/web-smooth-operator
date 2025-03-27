2f66a9264f113dcc6e1a88c5db46edb4130bef05
y-chernenko <c-ychernenko@touchtunes.com>
2025-03-27 20:37:42 +0200

    Cleanup

--- app/root.tsx
+++ app/root.tsx
@@ -18,14 +18,14 @@ export const links: LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="html">
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body className="body">
+      <body>
         {children}
         <ScrollRestoration />
         <Scripts />
